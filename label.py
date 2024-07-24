6124ad9ac9cff4b746fa825627cb931a968e5056
cailuzan <156872379+cailuzan@users.noreply.github.com>
2024-07-24 14:47:09 +0800

    Update label.py add some explainations

--- label.py
+++ label.py
@@ -1,24 +1,40 @@
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import os
 import sys
+
+tf.disable_v2_behavior()  # 禁用TensorFlow 2.x行为，启用1.x兼容模式
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 input_layer = 'DecodeJpeg/contents:0'
 output_layer = 'final_result:0'
 
-#Reading Image data from command line argument
-image = tf.gfile.FastGFile(sys.argv[1], 'rb').read()
+# 验证文件路径
+if len(sys.argv) < 2:
+    print("Please provide the image file path as an argument.")
+    sys.exit(1)
 
-#Reading labels file
-labels = [line.rstrip() for line in tf.gfile.GFile("labels_hotdog.txt")]
-with tf.gfile.FastGFile("graph_hotdog.pb", 'rb') as f:
+image_path = sys.argv[1]
+
+if not tf.io.gfile.exists(image_path):
+    print(f"File does not exist: {image_path}")
+    sys.exit(1)
+
+print(f'Reading image from: {image_path}')
+
+# 读取图像数据
+with tf.io.gfile.GFile(image_path, 'rb') as f:
+    image = f.read()
+
+# 读取标签文件
+labels = [line.rstrip() for line in tf.io.gfile.GFile("labels_hotdog.txt", 'r')]
+with tf.io.gfile.GFile("graph_hotdog.pb", 'rb') as f:
     g_def = tf.GraphDef()
     g_def.ParseFromString(f.read())
     tf.import_graph_def(g_def, name='')
 
 with tf.Session() as sess:
-    soft_tensor=sess.graph.get_tensor_by_name(output_layer)
+    soft_tensor = sess.graph.get_tensor_by_name(output_layer)
     predict, = sess.run(soft_tensor, {input_layer: image})
     top = predict.argsort()[-2:][::-1]
     for x in top:
